8fc70dc4258fddd65d9d0e1ec40bff15bb02d68b
Eugene <Zhenya6207@gmail.com>
2022-05-30 21:57:46 +0300

    report file send

--- src/books/reportBooks/report.controller.ts
+++ src/books/reportBooks/report.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get, Query, Response, StreamableFile } from "@nestjs/common";
-import { createReadStream, fstat } from "fs";
+import { createReadStream } from "fs";
 const fs = require('fs');
 const path = require('path');
+const Emmiter = require('events');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 import { join } from "path";
 import { ReportBookService } from "./report.service";
@@ -10,50 +11,124 @@ import { ReportBookService } from "./report.service";
 @Controller('')
 export class ReportBookController {
 
-    constructor(private reportService: ReportBookService) { }
+    constructor(private reportService: ReportBookService) {
+
+    }
 
     @Get()
     async getFile(@Response({ passthrough: true }) res): Promise<StreamableFile> {
-        const pathfolder = path.join(__dirname, '..', '..', '..', 'fileReport', 'test.csv');
-        const data = await this.reportService.getData([1000, 2000]);
-        console.log(data);
-        console.log(__dirname);
+        const pathFolder = path.join(__dirname, '..', '..', '..', 'fileReport');
+        const emmiter = new Emmiter();
+        const data = await this.reportService.getData([1000, 4000]);
 
-        // Промисификацию!!!
-        fs.mkdir(pathfolder, (err) => {
-            if (err) {
-                console.log('error create file');
-                //return;
-            }
-            console.log('folder create successful');
+        function checkFile() {
+            return new Promise((resolve, reject) => {
+                fs.access(path.join(pathFolder, 'fileReport.csv'), fs.F_OK, err => {
+                    if (err) {
+                        reject('file not exists')
+                    }
+                    resolve('file exists');
+                })
+            })
+        }
 
+        function createFile() {
+            return new Promise((resolve, reject) => {
+                fs.mkdir(pathFolder, err => {
+                    if (err) {
+                        reject('error create folder');
+                    }
+                    resolve('create folder successful');
+                })
+            })
+        }
+
+        function createCSVFile() {
             const csvWrite = createCsvWriter({
-                path: pathfolder,
+                path: path.join(pathFolder, 'fileReport.csv'),
                 header: [
-                    {id: 'title', title: 'Title'},
-                    {id: 'year', title: 'Year'},
-                    {id: 'author', title: 'Author'},
-                    {id: 'isbn', title: 'Isbn'},
+                    { id: 'title', title: 'Title' },
+                    { id: 'year', title: 'Year' },
+                    { id: 'author', title: 'Author' },
+                    { id: 'isbn', title: 'Isbn' },
                 ]
             })
 
-            csvWrite
-            .writeRecords(data)
-            .then(() => console.log(123));
-            /*
-            const writeStream = fs.createWriteStream(path.resolve(pathfolder, 'reportJSON.csv'));
-            data.forEach(item => writeStream.write(item))
-            writeStream.end(() => {
-                console.log('end write');
-            });
-            */
-        })
+            return csvWrite
+                .writeRecords(data)
+                .then(() => ('csv created'))
+                .catch(() => console.log('csv not created'))
+                
+        }
 
-        const file = createReadStream(join(process.cwd(), 'package.json'));
-        res.set({
-            'Content-Type': 'application/json',
-            'Content-Disposition': 'attachment; filename="package.csv"',
-        });
-        return new StreamableFile(file);
+        function sendFile(){
+            const file = createReadStream(join(pathFolder, 'fileReport.csv'));
+            res.set({
+                'Content-Type': 'application/json',
+                'Content-Disposition': 'attachment; filename="report.csv"',
+            });
+            return new StreamableFile(file);
+        }
+
+        try{
+            const check = await checkFile();
+            console.log(check);
+            if(check === 'file exists'){
+                const writeFile = await createCSVFile();
+                console.log(writeFile);
+                if(writeFile === 'csv created'){
+                    console.log(123);
+                    return sendFile();
+                } 
+            }
+            //return sendFile();
+            /*if(check === 'file exists from promise'){
+                console.log(check);
+                const createCSV = await createCSVFile();
+            }*/
+        } catch(message){
+            if(message === 'file not exists'){
+                const checkDir = await createFile();
+            }
+        }
+        
+        /*
+
+        await checkFile()
+            .then((message) => {
+                console.log(message);
+                return Promise.reject('file exists from promise');
+            })
+            .then((message) => {
+                console.log(message);
+                createCSVFile();
+            })
+            .catch((message) => {
+                if (message === 'file exists from promise') {
+                    console.log(message);
+                    createCSVFile();
+                } else if (message === 'file not exists'){
+                    createFile()
+                        .then(message => {
+                            console.log(1)
+                            console.log(message);
+                            createCSVFile();
+                        })
+                        .catch(message => {
+                            console.log(2)
+                            console.log(message);
+                            createCSVFile();
+                        })
+                }  
+            })
+            .catch((message) => {
+                console.log(message);
+                console.log('error: create file');
+            })
+
+            console.log(123123)
+            */
+        //return sendFile();
+
     }
 }
